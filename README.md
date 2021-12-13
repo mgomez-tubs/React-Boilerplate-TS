4e489d7585dd6c0c7871afe70841173388dbf223
Mauricio <m.gomezm@gmx.de>
2021-12-13 20:25:40 +0100

    Init

---
+++ README.md
@@ -0,0 +1,25 @@
+# Simple React Boilerplate
+
+Very simple React boilerplate with development hot reloading (see package.json).
+
+### Features
+- Hot reloading through react-refresh-webpack-plugin
+- Scss/ Sass support
+- Webpack 5 
+
+### Usage
+- Don't forget to install all dependencies first.
+    
+    `npm install`
+
+- To start the development server
+
+    `npm run start`
+
+
+
+
+
+
+
+
---
+++ webpack.config.js
@@ -0,0 +1,70 @@
+const ReactRefreshWebpackPlugin = require("@pmmmwh/react-refresh-webpack-plugin");
+const { BundleAnalyzerPlugin } = require("webpack-bundle-analyzer");
+
+module.exports = (env) => {
+  const isDevelopment = env.development || false;
+  const options = {
+    generateBundleAnalyzer : false
+  }
+  return {
+    mode: isDevelopment ? "development" : "production",
+    module: {
+      rules: [
+        {
+          test: /\.s[ac]ss$/i,
+          use: [
+            // Creates `style` nodes from JS strings
+            "style-loader",
+            // Translates CSS into CommonJS
+            "css-loader",
+            // Compiles Sass to CSS
+            "sass-loader",
+          ],
+        },
+        {
+          test: /\.(png|svg|jpe?g|gif)$/i,
+          use: [
+            {
+              loader: "file-loader",
+            },
+          ],
+        },
+        { 
+          test: /\.(woff|woff2|eot|ttf|otf)$/,
+          use: [
+            {
+              loader: 'file-loader',
+              options: {
+                outputPath: 'fonts',
+              }
+            }
+          ],
+        },
+        {
+          test: /\.(js|jsx)$/,
+          exclude: /node_modules/,
+          use: {
+            loader: require.resolve("babel-loader"),
+            options: {
+              plugins: [
+                isDevelopment && require.resolve("react-refresh/babel"),
+              ].filter(Boolean),
+            },
+          },
+        },
+      ],
+    },
+    devServer: {
+      hot: true,
+      contentBase: "./dist",
+    },
+    plugins: [
+      isDevelopment && new ReactRefreshWebpackPlugin(),
+      options.generateBundleAnalyzer && new BundleAnalyzerPlugin(),
+    ].filter(Boolean),
+    devtool: "eval-source-map",
+    optimization: {
+      usedExports: true,
+    }
+  };
+};
