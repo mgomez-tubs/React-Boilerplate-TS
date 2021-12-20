991c35f0a9358587443bf7e3de6c9dbd755cd40d
Mauricio <m.gomezm@gmx.de>
2021-12-20 01:43:56 +0100

    Init

---
+++ .storybook/main.js
@@ -0,0 +1,12 @@
+module.exports = {
+  core: {
+    builder: "webpack5",
+  },
+  stories: ["../src/**/*.stories.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
+  addons: [
+    "@storybook/addon-links",
+    "@storybook/addon-essentials",
+    "@storybook/preset-scss",
+  ],
+  framework: "@storybook/react",
+};
